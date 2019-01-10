f6452d761fda1d116573f1dd3441ed754a99bf44
elenapushkareva87 <46156427+elenapushkareva87@users.noreply.github.com>
2019-01-10 11:26:25 +0300

    HW2 added

---
+++ hw2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE> 
+<html lang="ru">
+    <head>
+        <?php  
+$animals = array(
+    "Africa" => array("Giraffa camelopardalis rothschildi", "Loxodonta"),
+    "Antarctica" => array("Mesonychoteuthis hamiltoni"),
+    "Australia" => array("Macropus","Threskiornis spinicollis"),
+    "Eurasia" => array("Pteromys volans", "Bison bonasus", "Apodemus agrarius"),
+    "North America" => array("Martes americana", "Bison bison"),
+    "South America" => array("Eunectes murinus")
+    );
+    
+        ?>
+        <title>
+            <p>
+                Жестокое обращение с животными 
+            </p>
+        </title>
+        <meta charset="utf-8">
+        <style>
+            body {
+                font-family: sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Исходный массив</h1>
+            <?php  ?>
+        
+    </body>
+</html>
